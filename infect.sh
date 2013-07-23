1d1eba357bb5b4e1bd53bbfc0ca50edc51315d35
Andrew Guenther <guenther.andrew.j@gmail.com>
2013-07-23 16:00:57 -0700

    Infect script now tries to install dependencies

--- infect.sh
+++ infect.sh
@@ -2,6 +2,16 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+command -v apt-get && INSTALL="sudo apt-get install "
+command -v yum && INSTALL="sudo yum install "
+
+while read dep; do
+   $INSTALL $dep
+done < dependencies.list
+
+git submodule init
+git submodule update
+
 # Link dotfiles
 for f in $DIR/link/*; do
    rm -rf ~/.${f##*/} 
