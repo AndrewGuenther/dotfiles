7024a36f4f4a9d159ec6e330740a38faf6d8bf78
Andrew Guenther <guenther.andrew.j@gmail.com>
2013-07-24 00:19:14 +0000

    Don't delete old dotfiles, just move them. Fixes #1

--- infect.sh
+++ infect.sh
@@ -12,8 +12,17 @@ sudo chsh -s /bin/zsh $USER
 git submodule init
 git submodule update
 
+# Create a directory to put old dotfiles
+if [ -d ~/.olddots ]; then
+   rm -rI ~/.olddots
+fi
+mkdir ~/.olddots
+
 # Link dotfiles
 for f in $DIR/link/*; do
-   rm -rf ~/.${f##*/} 
+   if [ -f ~/.${f##*/} ]; then
+      cp ~/.${f##*/} ~/.olddots/.${f##*/}
+   fi
+   rm ~/.${f##*/}
    ln -s $f ~/.${f##*/}
 done
